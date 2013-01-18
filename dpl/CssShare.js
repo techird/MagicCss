8d497c1eedd0a86d990982b6bcd5cbb2dc822fb8
techird <techirdliu@gmail.com>
2013-01-18 13:02:27 +0800

    展示平台更新
    1. 上方banner不使用fix
    2. 低版本浏览器bug修正

--- dpl/CssShare.js
+++ dpl/CssShare.js
@@ -1,29 +1,46 @@
 baidu(function(){
+    String.prototype.isEmpty = function () {
+        return !/\S/.test(this);
+    }
+    /**
+     * @description 自动识别字符串开头的缩进，缩进最顶级到行首，同时去除首位空行
+     */
     function fixTab(str) {
-        // var arr, i, line, spaces, min_spaces;
-        // str = str.replace(/\t/g, '    ');
-        // arr = str.split('\n');
-        // for (i in arr) {
-        //     line = arr[i];
-        //     if(!/\S/.test(line)) continue;
-        //     spaces = /^ +/.exec(line);
-        //     if(!min_spaces || spaces && spaces[0].length < min_spaces.length) min_spaces = spaces[0];
-        // }
-        // if(!min_spaces) return str;
-        // for (i = 0; i < arr.length; i++) {
-        //     arr[i] = arr[i].substr(min_spaces.length);
-        // }
-        // while(!/\S/.test(arr[0])) arr.shift();
-        // while(!/\S/.test(arr[arr.length-1])) arr.pop();
-        // return arr.join('\n');
+        if(!str) return;
+        var arr, i, line, spaces, min_spaces;
+
+        str = str.replace(/\t/g, '    ');
+        arr = str.split('\n');
+
+        while( arr[0].isEmpty() ) arr.shift();
+        while( arr[arr.length-1].isEmpty() ) arr.pop();
+
+        for ( i = 0; i < arr.length; ++i ) {
+            line = arr[i];
+            if( line.isEmpty() ) continue;
+            spaces = /^ +/.exec(line);
+            if( !spaces ) 
+                min_spaces = 0;
+            else if( min_spaces == undefined || spaces[0].length < min_spaces.length ) min_spaces = spaces[0];
+        }
+
+        if ( min_spaces ) for (i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].substr(min_spaces.length);
+        }
+
+        return arr.join('\n');
     }
     var $nav = baidu('<ul>').appendTo(baidu('<div class="nav">').appendTo('body'));
     var section_pos = [];
-    var padding_top = 130;
+    var padding_top = function(){
+        var pad = 30, header_height = baidu('div.header').height();
+        var value = header_height - baidu(window).scrollTop();
+        value < 0 && (value = 0);
+        return value + pad;
+    };
     function makeNavFor( $section ) {
         var section_top = $section.offset().top;
         var title = $section.children('h2').text();
-        if(!title) return;
         var $li = baidu('<li>').appendTo($nav);
         var $a = baidu('<a></a>')
             .text(title)
@@ -34,15 +51,17 @@ baidu(function(){
     }
     function onNavClick( e ) {
         var target = baidu(e.target).data('target');
-        baidu('body').scrollTop(target.offset().top-padding_top);
+        baidu(window).scrollTop(target.offset().top-padding_top());
+        baidu(window).scrollTop(target.offset().top-padding_top()); //执行两次，才可以正确计算
         updateCurrentNav();
     }
 
     var $sections = baidu('div.section');
     $sections.each(function(index, section){
         var $section = baidu(section);
-        // var $link = $section.children('link');
-        // var $example = $section.children('div.example-html');
+        var $link = $section.children('link');
+        var $example = $section.children('div.example-html');
+        if(!$example.length) return;
         // baidu.ajax({
         //     url: $link.attr('href'),
         //     type: 'GET',
@@ -53,24 +72,34 @@ baidu(function(){
         //         prettyPrint();
         //     }
         // });
-        // var html_code = fixTab($example.html());
-        // var $example_code = baidu('<pre>').text(html_code).addClass('prettyprint').addClass('html');
-        // $example.before($example_code);
+        var html_code = fixTab( $example.html() );
+        var $example_code = baidu('<pre>').text(html_code).addClass('prettyprint').addClass('html');
+        $example.before($example_code);
         makeNavFor($section);
     });
     function updateCurrentNav (){
-        var bt = baidu('body').scrollTop() + padding_top, range = 10;
+        var bt = baidu(window).scrollTop() + padding_top(), range = 10;
         $sections.each(function(index, section) {
             var $section = baidu(section);
             var st = $section.offset().top,
                 sh = $section.height();
             if( bt > st - range && bt < st + sh ) {
                 baidu('.nav a.current').removeClass('current');
-                $section.data('nav').addClass('current');
+                var $nav = $section.data('nav');
+                $nav && $nav.addClass('current');
             }
         });
     }
+    function fixNavPosition() {
+        var top = baidu('div.header').height() - baidu(window).scrollTop();
+        if(top < 0) top = 0;
+        if(baidu.browser.ie && baidu.browser.ie < 7) {
+            top += baidu(window).scrollTop();
+        }
+        baidu('div.nav').css('top', top + 30 + 'px');
+    }
     baidu(window).scroll(updateCurrentNav);
+    baidu(window).scroll(fixNavPosition).resize(fixNavPosition);
     updateCurrentNav();
     prettyPrint();
 });
